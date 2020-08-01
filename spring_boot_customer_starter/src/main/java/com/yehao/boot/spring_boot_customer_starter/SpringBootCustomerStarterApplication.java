4b0be304617cc31054558f780a87258558f903d3
LiuYong <ly1993110@163.com>
2020-08-01 15:36:14 +0800

    springboot自定义starter项目

---
+++ spring_boot_customer_starter/src/main/java/com/yehao/boot/spring_boot_customer_starter/SpringBootCustomerStarterApplication.java
@@ -0,0 +1,13 @@
+package com.yehao.boot.spring_boot_customer_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCustomerStarterApplication.class, args);
+    }
+
+}
---
+++ spring_boot_customer_starter/src/test/java/com/yehao/boot/spring_boot_customer_starter/SpringBootCustomerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yehao.boot.spring_boot_customer_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCustomerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
